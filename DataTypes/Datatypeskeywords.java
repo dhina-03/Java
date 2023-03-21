dd8bd5dd95eedf1f9e5807fbed135908c8bcafc2
dhina-03 <128469366+dhina-03@users.noreply.github.com>
2023-03-21 15:19:38 +0530

    Add files via upload

---
+++ DataTypes/Datatypeskeywords.java
@@ -0,0 +1,31 @@
+package java1;
+
+public class Datatypeskeywords {
+	public static void main(String[] args) {
+			
+			byte b = 127; //-128 to 127.
+			short s = 32767; // -32768 to 32768.
+			int n = 868768686;
+			long l = 676768768442422424L;
+			float f = 5.8f;
+			double d = 5.88;
+			boolean bo = true;
+			char c ='A';
+			
+			System.out.println(b);
+			System.out.println(s);
+			System.out.println(n);
+			System.out.println(l);
+			System.out.println(f);
+			System.out.println(d);
+			System.out.println(bo);
+			System.out.println(c);
+			
+			
+			
+			
+		}
+
+	}
+
+
